418e5e58d84d0269c471d10b845fecdf093d3890
jrheard <jrheard@cs.stanford.edu>
2023-11-02 12:25:01 -0700

    changes for keel

--- gen_counts.py
+++ gen_counts.py
@@ -7,16 +7,16 @@ TARGET_NUM_WORDS = 50_000
 
 # requirements:
 # must hit 50k by end of month
-# should be unpredictable, vary by ~500 words either direction each day
+# should be unpredictable, vary by ~1500 words either direction each day
 def generate_number_of_words_per_day() -> list[int]:
-    result = []
-    for i in range(NUM_DAYS_IN_NOVEMBER):
+    result = [1603] # generated 1603 on the first day and then keel asked for the algo to be changed for future days
+    for i in range(NUM_DAYS_IN_NOVEMBER - 1):
         if i == NUM_DAYS_IN_NOVEMBER - 1:
             result.append(TARGET_NUM_WORDS - sum(result))
             break
 
         on_track_number = (TARGET_NUM_WORDS - sum(result)) / (NUM_DAYS_IN_NOVEMBER - i)
-        daily_number = math.ceil(on_track_number + random.randrange(-500, 500))
+        daily_number = math.ceil(on_track_number + random.randrange(-1500, 1500))
         result.append(daily_number)
 
     return result
